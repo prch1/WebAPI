d049fddca5393816096482d752feddfd5f6e7a6f
Paulo Ricardo <pauloricardoh@hotmail.com>
2021-06-06 18:52:28 -0300

    Aula 2 - Filtro,Ordenacao ,Pag

--- Alura.WebAPI/Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ Alura.WebAPI/Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Alura.WebAPI.Api.Modelos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
@@ -21,10 +22,19 @@ namespace Alura.ListaLeitura.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaDeLivros()
+        public IActionResult ListaDeLivros(
+            [FromQuery] LivroFiltro filtro,
+            [FromQuery] LivroOrdem ordem,
+            [FromQuery] LivroPaginacao paginacao)
         {
-            var lista = _repo.All.Select(l => l.ToApi()).ToList();
-            return Ok(lista);
+            var livroPaginado = 
+                _repo.All
+                .AplicarFiltro(filtro)
+                 .AplicarOrdem(ordem)
+                .Select(l => l.ToApi())
+                .ToLivroPaginado(paginacao);
+
+            return Ok(livroPaginado);
         }
 
         [HttpGet("{id}")]
---
+++ Alura.WebAPI/Alura.WebAPI.Api/Modelos/LivroOrdem.cs
@@ -0,0 +1,28 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+
+namespace Alura.WebAPI.Api.Modelos
+{
+    public static class LivroOrdemExtensions
+    { 
+       public static IQueryable<Livro> AplicarOrdem(this IQueryable<Livro> query, LivroOrdem ordem)
+        {
+            if(ordem != null)
+            {
+                query = query.OrderBy(ordem.OrdenarPor);
+            }
+
+            return query;
+        }
+    }
+
+    
+    public class LivroOrdem
+    {
+        public string OrdenarPor { get; set; } = "titulo asc";
+    }
+}
